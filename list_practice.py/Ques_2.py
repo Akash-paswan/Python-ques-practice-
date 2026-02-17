d38164d0fe123d20b458e95a654e79a1803489a8
Akash-paswan <ap6655713@gmail.com>
2026-02-17 11:37:39 +0530

    create a lisrt and print theie first name and last name

---
+++ list_practice.py/Ques_2.py
@@ -0,0 +1,4 @@
+# question 2: Write a Python program that creates a list of numbers and prints the first and last elements of the list.
+num = [5,6,1,4,25,89]
+print( "first num is :",num[0])
+print( "second num is :",num[5])
