063ca3e9c271aff901e98f948dd3742cddbd2066
Akash-paswan <ap6655713@gmail.com>
2026-02-17 11:18:26 +0530

    take name from keyborad and find its length

--- String_andConditional.py/Ques_14.py
+++ String_andConditional.py/Ques_14.py
@@ -1,4 +1,4 @@
-    
+    #  question 14: Write a Python program that takes a word as input and checks if it ends with a vowel or a consonant.
 word = input("Enter a word: ").lower()
 
 if word[-1] in "aeiou":
--- String_andConditional.py/Ques_15.py
+++ String_andConditional.py/Ques_15.py
@@ -1,3 +1,4 @@
+#  question 15: Write a Python program that takes a name as input and checks if the length of the name is even or odd.
 name = input("enter name :")
 if len(name) %2 == 0:
     print("name is even :")
