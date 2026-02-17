ed3e16dd8acc867c9fcefdf64b6cae2114ea4d6d
Akash-paswan <ap6655713@gmail.com>
2026-02-17 12:11:45 +0530

    create a list and check 50 value is present or nit in a listr

---
+++ list_practice.py/Ques_5.py
@@ -0,0 +1,6 @@
+#  question 5: Write a Python program that creates a list of numbers and checks if a specific number is present in the list.
+num = [5,6,1,50,25,89]
+if 50 in num:
+    print("number is present in list :")
+else :
+    print("number is not present in list :")
