d70076156c6a774e908c549084ef7fada9f95422
Akash-paswan <ap6655713@gmail.com>
2026-02-17 12:06:25 +0530

    create a list and reverse their value

--- list_practice.py/Ques_3.py
+++ list_practice.py/Ques_3.py
@@ -1,4 +1,4 @@
-# question 3: 
+# question 3: Write a Python program that creates a list of numbers and adds a new number to the end of the list.
 num = [5,6,1,4,25,89]
 print( "add nem number at end of list :",num.append(45))
 print(num)
---
+++ list_practice.py/Ques_4.py
@@ -0,0 +1,6 @@
+# Question 4: Write a Python program that creates a list of numbers and prints the list in reverse order.
+num = [5,6,1,4,25,89]
+# print(num[-1:-7:-1])
+            # (start:stop:step)    step ,means reverse the list
+print(num[::-1])
+
