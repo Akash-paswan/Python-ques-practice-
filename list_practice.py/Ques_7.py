725ab4a593c1718f7df4691829e07f98dc93cb07
Akash-paswan <ap6655713@gmail.com>
2026-02-18 10:36:06 +0530

    create a list and find second smallest number

---
+++ list_practice.py/Ques_7.py
@@ -0,0 +1,9 @@
+# Ques 7: Write a Python program to find the second largest number in a list.
+num = [10,20,30,4,5,8,]
+num.sort()
+print("seconmd largest number is:",num[1])
+
+
+
+#   smallest number = index [0]
+#  second smallest number = index [1]  karte hau hm list me 
