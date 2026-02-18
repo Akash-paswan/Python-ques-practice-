49243f5dd69c2713eec5349a1641a9019def4eb2
Akash-paswan <ap6655713@gmail.com>
2026-02-18 10:50:17 +0530

    take 5 number from user and find therie average value

---
+++ list_practice.py/Ques_9.py
@@ -0,0 +1,16 @@
+num = []
+
+
+for i in range(5):
+    n = int(input("Enter a number: "))
+    num.append(n)
+    
+    average = sum(num) / len(num)
+    print("numbers are:",num)
+    print("average is:",average)
+           
+           
+      
+        #  nums.append(n) kaa mtlab niche likha haun 
+# Number ko list me add kar rahe hain.  
+        
