6216d71b561edeb749b5c5078121a49afb806f06
Akash-paswan <ap6655713@gmail.com>
2026-02-18 11:42:37 +0530

    create a list and count which number is greater than 10

---
+++ list_practice.py/Ques_11.py
@@ -0,0 +1,7 @@
+#  ques 11: Write a Python program to count the number of elements in a list that are greater than a specified value.
+nums = [5,15,25,8,12]
+count = 0
+for n in nums :
+    if n > 10:
+        count += 1 
+print("Count of numbers greater than 10:", count)
