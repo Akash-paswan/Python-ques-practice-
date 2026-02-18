ad1692bd89dc757bcbacf3ea3b10661c87b77168
Akash-paswan <ap6655713@gmail.com>
2026-02-18 11:58:32 +0530

    crea a list and find minimum and maximum vakiue in a list

---
+++ list_practice.py/Ques_12.py
@@ -0,0 +1,16 @@
+nums = [10, 5, 20, 3]
+#   isme max pr main ek variable hai 
+maximum = nums[0]    # kuki hume comparison start karne ke liye
+minimum = nums[0]
+                        # list ka koi ek real number chahiye
+
+                             # Aur sabse easy hai pehla element (nums[0]).
+
+for n in nums:
+    if n > maximum:
+        maximum = n
+    if n < minimum:
+        minimum = n
+
+print("Max:", maximum)
+print("Min:", minimum)
