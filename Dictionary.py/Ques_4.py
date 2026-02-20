d892db254dc74b9d950786c5e179b6540c1c9c00
Akash-paswan <ap6655713@gmail.com>
2026-02-20 13:17:58 +0530

    Check whether the key math exists in the dictionary or not.

---
+++ Dictionary.py/Ques_4.py
@@ -0,0 +1,7 @@
+#  ques 4 Check whether the key "math" exists in the dictionary or not
+
+marks = {"math":80,"english":85,"science":90}
+if "math" in marks :
+    print("maths exists :")
+else :
+    print("math does not exist :")
