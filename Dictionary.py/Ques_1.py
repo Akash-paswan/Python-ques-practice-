a61f61c3e2e0266606e7e032f3a9b6c5574cd87f
Akash-paswan <ap6655713@gmail.com>
2026-02-20 07:18:30 +0530

    create a dictionary that stores a students name ,age ,course and print only the age of the student

---
+++ Dictionary.py/Ques_1.py
@@ -0,0 +1,9 @@
+#  ques 1 create a dictionary that stores a students name ,age ,course and print only the age of the student
+
+student = {
+    "name": "Akash",
+    "age": 20,
+    "course": "B.Tech",
+}
+
+print(student["age"])
