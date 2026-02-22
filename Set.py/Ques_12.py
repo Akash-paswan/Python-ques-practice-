e634c8b4ec4ee2fd1a82957acd003521e074930e
Akash-paswan <ap6655713@gmail.com>
2026-02-22 12:22:16 +0530

    take 5 number from user and print into a set

---
+++ Set.py/Ques_12.py
@@ -0,0 +1,10 @@
+num = set()    #  empty set banaya
+
+
+for i in range(5):
+    n = int(input("Enter the number: "))
+    num.add(n)     #  set me number add kar diya
+    
+    
+print(num)    
+    
