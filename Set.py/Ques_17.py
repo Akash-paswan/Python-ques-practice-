76e302771457cf0b439b1d8ac905f23bda0824ad
Akash-paswan <ap6655713@gmail.com>
2026-02-22 12:57:17 +0530

    use set comprehension to create a set of square from 1 to 10

--- Set.py/Ques_17.py
+++ Set.py/Ques_17.py
@@ -1,3 +1,3 @@
 a = {1,2,3,4,5,6,7}
 b = {8,9,10,11}
-print(a.isdisjoint (b))    # disjoint means koi commom element na hi set me 
+print(a.isdisjoint (b))    # disjoint means koi commom element na ho set me 
---
+++ Set.py/Ques_18.py
@@ -0,0 +1,4 @@
+#  ques 18  Use set comprehension to create a set of squares from 1 to 10.
+
+squares = {x*x for x in range(1, 11)}
+print(squares) 
