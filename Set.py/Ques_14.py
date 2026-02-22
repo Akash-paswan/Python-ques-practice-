12cbcf992c54043919485736fc80644095f0a089
Akash-paswan <ap6655713@gmail.com>
2026-02-22 12:39:29 +0530

    diff between remove() and discard()

---
+++ Set.py/Ques_14.py
@@ -0,0 +1,9 @@
+#  ques 14  diff between remove and discard method in set
+
+s = {1,2,3}
+s.remove(2)   #  remove method se agar element nahi hoga to error aayega
+s.discard(4)    #  discard method se agar element nahi hoga to error nahi aayega
+
+
+print(s)
+ 
