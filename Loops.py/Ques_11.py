a9d0510676d45c3171828cb686d912a8ed9eb21e
Akash-paswan <ap6655713@gmail.com>
2026-02-25 00:04:42 +0530

    using a while loop create a pattern

---
+++ Loops.py/Ques_11.py
@@ -0,0 +1,14 @@
+i = 1 
+while i <=5:
+    print("*" *i)
+    i += 1
+      
+      
+#       3️⃣ Important Line 👇
+# print("*" * i)
+
+# Yaha "*" ko i times multiply kar rahe hain.
+
+# Python me:
+
+# "*" * 3  →  ***
