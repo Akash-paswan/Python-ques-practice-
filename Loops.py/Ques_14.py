ba7c6a5429c8432d16a3c83a78c751c96fbeb6dd
Akash-paswan <ap6655713@gmail.com>
2026-02-25 11:46:25 +0530

    Keep asking numbers from user until user enters 0, then print total sum.

---
+++ Loops.py/Ques_14.py
@@ -0,0 +1,10 @@
+#  ques keep asking numbers from user until user enters 0, then print total sum.
+
+total = 0   # variaqlbe hai yre 
+num = 1 
+
+while  num != 0:
+    num = int (input ("enter a number :"))
+    total += num 
+    
+print("total sum", total)  
