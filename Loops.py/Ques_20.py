8067c0ede348164b998ef86ee474d42071178355
Akash-paswan <ap6655713@gmail.com>
2026-02-25 22:51:53 +0530

    ATM machine simulation (withdraw until balance becomes 0

---
+++ Loops.py/Ques_20.py
@@ -0,0 +1,12 @@
+balance = 1000
+
+while balance > 0:
+    withdraw = int(input("Enter amount to withdraw: "))
+    
+    if withdraw <= balance:
+        balance -= withdraw
+        print("Remaining balance:", balance)
+    else:
+        print("Insufficient balance")
+
+print("Balance is 0. Thank you!")
