5141ab238c684099ae7b6e76981f144f0adfd963
Akash-paswan <ap6655713@gmail.com>
2026-02-27 07:20:46 +0530

    find largest numbver in a list

---
+++ Loops.py/For_loops.py/Ques_10.py
@@ -0,0 +1,8 @@
+numbers = [10, 45, 7, 89, 23]
+largest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        largest = num
+
+print("Largest number:", largest) 
--- Loops.py/For_loops.py/ques_9.py
+++ Loops.py/For_loops.py/ques_9.py
@@ -1,3 +1,5 @@
+#  ques print elements of a list
+
 numbers = [10,2,30]
 for num in numbers :
     print(num)
