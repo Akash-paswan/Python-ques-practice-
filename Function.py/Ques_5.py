faf3a6972edf6fd015d201ead11aecbe665890ec
Akash-paswan <ap6655713@gmail.com>
2026-02-28 11:39:44 +0530

    Write a function that takes name and age as arguments and prints them using keyword arguments.

---
+++ Function.py/Ques_5.py
@@ -0,0 +1,9 @@
+def students(name,age):
+    print("name:",name)
+    print("age:",age)
+    
+students( age = 20 ,name = "akash")    # yah nkeyword argument hai jisme name aur uski value likhna hota hai 
+# students("Akash",20)      # normal argument 
+
+
+#   keyword argument me order important nhi hota hai
